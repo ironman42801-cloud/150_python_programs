9e22561e73fd944859bd4cc975fc1f74943117c7
ironman <ironman42801@example.com>
2026-06-30 22:01:58 +0530

    fix: output in 06

--- Fundamentals(1-15)/06_multiple_assignment.py
+++ Fundamentals(1-15)/06_multiple_assignment.py
@@ -6,9 +6,9 @@ to a multiple variables
 x = y = z = 10
 a, b, c = 3, 4, 5
 print(x, y, z)
-#output:- 10, 10, 10
+#output:- 10 10 10
 print(a, b, c)
-#output:- 3, 4, 5
+#output:- 3 4  5
 
 #Real life example
 #Same section students
@@ -17,6 +17,6 @@ student_1 = student_2 = student_3= "Section 1"
 student_4, student_5 = "Section 2", "Section 3"
 
 print(student_1, student_2, student_3)
-#output:- Section 1, Section 1, Section 1
+#output:- Section 1 Section 1 Section 1
 print(student_3, student_4, student_5)
-#output:- Section 1, Section 2, Section 3
+#output:- Section 1 Section 2 Section 3
